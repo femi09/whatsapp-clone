63523b9cfdded1400aaa49fdc1f30ccdee04e53a
Femi Omotayo <omotayooluwafemi66@gmail.com>
2020-12-08 13:46:26 +0100

    created the user interface for the chat component for users to send and recieve messages

--- src/Chat/index.jsx
+++ src/Chat/index.jsx
@@ -1,11 +1,64 @@
-import React from 'react'
+import React from "react";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import SearchOutlined from "@material-ui/icons/SearchOutlined";
+import AttachFile from "@material-ui/icons/AttachFile";
+import InsertEmoticonIcon from "@material-ui/icons/InsertEmoticon";
+import MicIcon from "@material-ui/icons/Mic";
+import { Avatar, IconButton } from "@material-ui/core";
+import "./index.css";
 
 const Chat = () => {
-    return (
-        <div className="chat">
-            <h1>I am the chat</h1>
+  return (
+    <div className="chat">
+      <div className="chat__header">
+        <Avatar />
+        <div className="chat__headerInfo">
+          <h3>Room name</h3>
+          <p>Last seen at...</p>
         </div>
-    )
-}
+        <div className="chat__headerRight">
+          <IconButton>
+            <SearchOutlined />
+          </IconButton>
+          <IconButton>
+            <AttachFile />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </div>
+      </div>
 
-export default Chat
+      <div className="chat__body">
+        <p className="chat__message">
+          <span className="chat__name">Femi</span>
+          This is a whatsapp message
+          <span className="chat__timestamp">{new Date().toUTCString()}</span>
+        </p>
+
+        <p className="chat__message chat__reciever">
+          <span className="chat__name">Femi</span>
+          This is a whatsapp message
+          <span className="chat__timestamp">{new Date().toUTCString()}</span>
+        </p>
+
+        <p className="chat__message">
+          <span className="chat__name">Femi</span>
+          This is a whatsapp message
+          <span className="chat__timestamp">{new Date().toUTCString()}</span>
+        </p>
+      </div>
+
+      <div className="chat__footer">
+        <InsertEmoticonIcon />
+        <form action="">
+          <input type="text" placeholder="Type a message" />
+          <button type="submit">Send a message</button>
+        </form>
+        <MicIcon />
+      </div>
+    </div>
+  );
+};
+
+export default Chat;
